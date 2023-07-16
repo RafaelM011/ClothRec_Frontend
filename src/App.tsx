f7543ff459207e1e8df432f0df29def4e7c8ed14
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-16 19:58:30 -0400

    <FIX> eslint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react"
+import { useState } from 'react'
 
-function App() {
+function App (): JSX.Element {
   const [imgInput, setImgInput] = useState<string>()
-  const [ hidden, setHidden] = useState("hidden")
+  const [hidden, setHidden] = useState('hidden')
 
   return (
     <div className="w-full h-fit min-h-[100vh] bg-gradient-to-r from-violet-500 to-fuchsia-500 flex justify-center">
@@ -11,10 +11,10 @@ function App() {
       </div>
       <div className="w-8/12 max-w-[450px] h-fit mt-[30vh]">
         <div className="w-full rounded-full py-2 px-8 bg-gradient-to-r from-fuchsia-500 to-violet-500">
-          <input className="w-full my-4 outline-none" onChange={(event: React.ChangeEvent<HTMLInputElement>) => setImgInput(event.target.value)}/>
+          <input className="w-full my-4 outline-none" onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setImgInput(event.target.value) }}/>
         </div>
         <div className="my-4">
-          <img className={`${hidden}`} src={imgInput} alt="recog-img" onLoad={() => setHidden("block")} onError={() => setHidden("hidden")}/>
+          <img className={`${hidden}`} src={imgInput} alt="recog-img" onLoad={() => { setHidden('block') }} onError={() => { setHidden('hidden') }}/>
         </div>
       </div>
       <div>
