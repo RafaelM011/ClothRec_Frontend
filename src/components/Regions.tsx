563bd69ae6d8a1a6584f5febc36afc174e4004a8
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-18 19:41:33 -0400

    <FEAT> Regions info feature shows the differente results

--- src/components/Regions.tsx
+++ src/components/Regions.tsx
@@ -6,14 +6,18 @@ interface Props {
 
 export const Regions: React.FC<Props> = ({ regions }): JSX.Element => {
   return (
-    <div className='bg-white h-fit w-2/12 mt-[30vh]'>
-      <h1 className='text-xl font-semibold text-center'> Regions found </h1>
+    <div className='h-fit w-3/12 max-w-[200px] mt-[30vh] py-4 px-2 border-violet-500 border-r-4 border-b-4 rounded-3xl'>
+      <h1 className='text-xl font-semibold text-center text-white mb-4'> Regions found </h1>
       <ul>
         { regions.map(region => {
-          const { id } = region
-          console.log(region)
+          const { id, data, value } = region
+          const { concepts } = data
           return (
-            <li key={id}> hi </li>
+            <li key={id} className='flex gap-2'>
+              <input type='checkBox'/>
+              <span className='text-lg font-medium text-white'> {concepts[0].name} </span>
+              <span className='grow text-end text-lg font-medium text-white'> {Math.floor(value * 100)}% </span>
+            </li>
           )
         })}
       </ul>
