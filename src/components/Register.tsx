ff8167bb28f23cda1c9681c0c258c48d659167e6
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-24 23:30:14 -0400

    <FEAT> register modal update div box

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,9 @@
 export const Register: React.FC = (): JSX.Element => {
   return (
-        <div className="absolute top-0 w-4/12 min-w-[400px] h-fit bg-white text-center">
-            <h1> Register </h1>
+        <div className="absolute top-0 bottom-0 left-0 right-0">
+            <div className="relative top-[200px] mx-auto w-4/12 w-min-[400px] h-fit text-center shadow-[0px_0px_4px_3px_rgba(0,0,0,0.3)] rounded-2xl">
+                <h1 className="font-bold text-2xl"> Register </h1>
+            </div>
         </div>
   )
 }
